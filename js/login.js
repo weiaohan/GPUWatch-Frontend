46ab3e8f772fbd88e73c00be7f83f0d46e5d10a3
weiaohan <380629654@qq.com>
2019-05-31 18:15:07 +0800

    fix login

--- js/login.js
+++ js/login.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
-    var data = {}
-    data.email = $("#inputEmail").val()
-    data.pwd = $("inputPassword").val()
-    console.log(data)
-    $.ajax({
-        url: "http://211.83.111.222/api/login",
-        type: "post",
-        data: data,
-        success: function(result) {
-            if(result == -1) {
-                alert("用户不存在！")
-            } else if (result == -2) {
-                alert("密码错误！")
-            } else {
-                var name = result.name
-                window.location.href = "http://211.83.111.222?name=" + name
+    $("#login").submit(function() {
+        var data = {}
+        data.email = $("#inputEmail").val()
+        data.pwd = $("inputPassword").val()
+        console.log(data)
+        $.ajax({
+            url: "http://211.83.111.222/api/login",
+            type: "post",
+            data: data,
+            success: function(result) {
+                if(result == -1) {
+                    alert("用户不存在！")
+                } else if (result == -2) {
+                    alert("密码错误！")
+                } else {
+                    var name = result.name
+                    window.location.href = "http://211.83.111.222?name=" + name
+                }
             }
-        }
+        })
     })
 });
